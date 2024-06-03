ac320c223aab5426d1db0ce7470dc9cf1eccf7bf
xxsimoxx <simone@gieffeedizioni.it>
2024-06-03 10:32:40 +0200

    Add build files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2b45cb53c254b590bfb1dc0fae80189177d1bcbc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2b45cb53c254b590bfb1dc0fae80189177d1bcbc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'splitbrain/php-qrcode' => array(
+            'pretty_version' => '0.1',
+            'version' => '0.1.0.0',
+            'reference' => 'c33506c5b7c1e71399da6fa917d69636cf37cb1f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../splitbrain/php-qrcode',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
